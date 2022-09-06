4816d502c560fd1a0e8d3e976f536aca0daf1e0a
RagingCODfish <zachary.uptin@me.com>
2022-09-06 16:05:12 +1000

    Initial Commit

---
+++ BowdensOwn/BowdensOwnApp.swift
@@ -0,0 +1,17 @@
+//
+//  BowdensOwnApp.swift
+//  BowdensOwn
+//
+//  Created by Zach Uptin on 6/9/2022.
+//
+
+import SwiftUI
+
+@main
+struct BowdensOwnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BowdensOwn/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BowdensOwn
+//
+//  Created by Zach Uptin on 6/9/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
